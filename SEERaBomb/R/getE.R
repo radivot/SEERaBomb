24712a69ee7f4af84b0a862d721f041cd93a4dbc
radivot <radivot@lri-106018.attlocal.net>
2015-01-14 16:26:43 -0500

    delete old getE and tsx and move getE2 and tsx2 into these function names.

--- SEERaBomb/R/getE.R
+++ SEERaBomb/R/getE.R
@@ -1,20 +1,29 @@
-getE=function(LPYM,L2D) { 
-  getEinner=function(PYM,L2D) {
-    #   getE=function(PYM) {
-    #   oldPY=apply(PYM[86:126,],2,sum)
-    #   PYM=rbind(PYM[-(86:126),],"90"=oldPY)
-    PYM=PYM[-c(1:14,86:126),]
-    newData=melt(PYM)
-    names(newData)<-c("age","year","py")
-    newData=cbind(newData)
-    #   newData=cbind(newData,sex="female")
-    picks=names(L2D)
-    sapply(picks,function(x) sum(exp(predict(L2D[[x]],newData))) )
-  } 
-  ptm <- proc.time()
-  EL=lapply(LPYM,getEinner,L2D)
-  E=t(sapply(EL,"("))
-  print(proc.time() - ptm) 
-  E
+getE=function(LPYM,D,ageStart,ageEnd,yearEnd,cancerS,picks) {
+  EI=split(D,D$cancer) # expected incidences per 100,000 py, broken down by cancers
+  EI=lapply(EI,function(x) acast(x, age~year, value.var="Eincid")) # convert 3 cols of df to matrices
+#    EI=lapply(EI,function(x) x[,-1])
+  if (ageStart>=1) LPYM=lapply(LPYM,function(x) x[-c(1:ageStart,(ageEnd+1):126),]) else
+  LPYM=lapply(LPYM,function(x) x[-c((ageEnd+1):126),])
+#   LPYM=lapply(LPYM,function(x) x[-c(1:15,86:126),-dim(x)[2]])
+  LPYM[["CMML"]]=LPYM[["CMML"]][,-c(1:13)] # shave off early years when wasn't included in SEER
+  LPYM[["MDS"]]=LPYM[["MDS"]][,-c(1:28)]
+#    picks=names(EI)
+  (E=matrix(0,nrow=length(cancerS),ncol=length(picks),dimnames=list(firstCanc=cancerS,secondCanc=picks)))
+  for (i in cancerS) {   #i loop on first cancers
+    PY=LPYM[[i]]
+    for (j in picks) # j loop on second cancers
+    { 
+      if (i=="CMML" & !j%in%c("CMML","MDS"))  E[i,j]=sum(PY*EI[[j]][,as.character(1986:yearEnd)])
+      if (i=="CMML" & j=="CMML")  E[i,j]=sum(PY*EI[[j]])
+      if (i=="CMML" & j=="MDS" )  E[i,j]=sum(PY[,as.character(2001:yearEnd)]*EI[[j]])
+      if (i=="MDS" & !j%in%c("CMML","MDS"))  E[i,j]=sum(PY*EI[[j]][,as.character(2001:yearEnd)])
+      if (i=="MDS" & j=="CMML")  E[i,j]=sum(PY*EI[[j]][,as.character(2001:yearEnd)])
+      if (i=="MDS" & j=="MDS" )  E[i,j]=sum(PY*EI[[j]])
+      if (!i%in%c("CMML","MDS") & j=="MDS" )  E[i,j]=sum(PY[,as.character(2001:yearEnd)]*EI[[j]])
+      if (!i%in%c("CMML","MDS") & j=="CMML" )  E[i,j]=sum(PY[,as.character(1986:yearEnd)]*EI[[j]])
+      if (!i%in%c("CMML","MDS") & !j%in%c("CMML","MDS") )  E[i,j]=sum(PY*EI[[j]])
+    }
+  }   
+  E/1e5
 }
 
--- SEERaBomb/R/tsx.R
+++ SEERaBomb/R/tsx.R
@@ -1,40 +1,49 @@
-tsx=function(seerSet,L2D,brks=c(0,2,5),bfn="", outDir="~/Results") { # FL for file list
+tsx=function(seerSet,brks=c(0,2,5),trts=NULL){ #, outDir="~/Results",txt=NULL) { # FL for file list
   surv=yrdx=modx=db=casenum=radiatn=cancer=trt=yrbrth=agedx=L2D=NULL 
   print(binS<-levels(cut(brks+0.1,breaks=c(brks,100)))) #this is just to make a vector of tsx interval/row names 
-#   load(FL$fL)   # bring in L2D
-#   load(FL$fS)   # bring in seerSet
-#   bf=FL$f  # f holds the base filename
-#   seerSet
-  canc=seerSet$canc  
   ptm <- proc.time()
-  trts=levels(canc$trt)
-  print(trts)
-  L=list()
-  for (R in trts) 
-  { 
-    #     R="rad"
-    print(R)
-    mids=NULL
-    Obs=NULL
-    Exp=NULL
-    for (bin in binS) 
-    {
-#       (bin=binS[1])
-      print(bin)
-      binIndx=getBinInfo(bin,binS)["index"]
-      L=post1PYO(canc,brks,binIndx,Trt=R )
-      Exp[[bin]]=getE(L$LPYM,L2D)
-      Obs[[bin]]=L$O
-      mids=c(mids,L$mid)
-    } # loop on tsx bins
-    L[[R]]$mids=mids
-    L[[R]]$Obs=Obs
-    L[[R]]$Exp=Exp
-  } # loop on R
-  fn=paste0(outDir,"/",bfn,"b",paste(brks,collapse="_"),".RData")
-  cat("Writing L to ",fn,"\n")
-  save(L,file=fn)
+  L=with(seerSet, {
+    if (is.null(trts)) trts=levels(canc$trt)
+    seerSet$trts=trts
+    yearEnd=max(popsa$year)
+    print(trts)
+    L=list()
+    for (R in trts) 
+    { 
+      #     R="rad"
+      print(R)
+      mids=NULL
+      Obs=NULL
+      Exp=NULL
+      PyM=NULL
+      for (bin in binS) 
+      {
+        #       (bin=binS[1])
+        print(bin)
+        binIndx=getBinInfo(bin,binS)["index"]
+        L1=post1PYO(canc,brks,binIndx,Trt=R )
+        #         print(D)
+        Exp[[bin]]=getE(L1$LPYM,D,ageStart,ageEnd,yearEnd,cancerS,picks)
+        Obs[[bin]]=L1$O[cancerS,picks]
+        PyM[[bin]]=L1$PY
+        mids=c(mids,L1$binMidPnt)
+      } # loop on tsx bins
+      L[[R]]$mids=mids
+      L[[R]]$Obs=Obs
+      L[[R]]$Exp=Exp
+      L[[R]]$PyM=PyM
+    } # loop on R
+    L
+  })
+  tsxn=paste0("b",paste(brks,collapse="_"))
+#   seerSet$bfn=paste0(seerSet$bfn,paste0("b",paste(brks,collapse="_")),txt)
+  if(is.null(seerSet$L)) seerSet$L=list() # set to a blank list if it was never filled
+  seerSet$L[[tsxn]]=L
+#   fn=paste0(outDir,"/",seerSet$bfn,".RData")
+#   cat("Writing L to ",fn,"\n")
+#   save(L,file=fn)
+#   seerSet$fL=fn
   print(proc.time() - ptm)
-  L
+  seerSet
 }
 
