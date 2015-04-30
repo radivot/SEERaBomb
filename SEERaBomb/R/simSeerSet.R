506f5518634b4202c926d5df4e192414b91e46db
radivot <radivot@lri-106018.ccf.org>
2015-04-30 17:14:14 -0400

    move to runif for survival times

--- SEERaBomb/R/simSeerSet.R
+++ SEERaBomb/R/simSeerSet.R
@@ -1,4 +1,4 @@
-simSeerSet<-function(N=2e9,yearEnd=2012,ka=1e-5,kb=0.04,Ab=1e-5,Rba=1e-3,delay=1,period=4,shape=4) {
+simSeerSet<-function(N=2e9,yearEnd=2012,ka=1e-5,kb=0.04,Ab=1e-5,Rba=1e-3,delay=1,period=4,shape=1) {
   #   agedx=age=age86=canc=yrdx=sex=race=surv=modx=yrbrth=NULL 
   cancers=NULL 
   data(stdUS)
@@ -6,7 +6,7 @@ simSeerSet<-function(N=2e9,yearEnd=2012,ka=1e-5,kb=0.04,Ab=1e-5,Rba=1e-3,delay=1
     shape=1
     print("Warning: shape must be 1 or higher. It was less and is being set to 1. ")
   }
-#   N=2e9;yearEnd=2012;ka=1e-5;kb=0.04;Ab=1e-5;Rba=1e-3;delay=1;period=4;shape=1
+#   N=2e9;yearEnd=2012;ka=1e-5;kb=0.04;Ab=1e-5;Rba=1e-3;delay=1;period=4;shape=1;library(dplyr)
   popsa=merge(data.frame(age=0.5:99.5),data.frame(year=1973:yearEnd))
   popsa$py=N/40*SEERaBomb::stdUS$prop[round(popsa$age+0.5)]
   #   sum(popsa$py)
@@ -14,6 +14,7 @@ simSeerSet<-function(N=2e9,yearEnd=2012,ka=1e-5,kb=0.04,Ab=1e-5,Rba=1e-3,delay=1
   #   tail(popsa)
   A=cbind(popsa[,1:2],cancers=rpois(dim(popsa)[1],ka*popsa$age*popsa$py))
   head(A)
+  sum(A$cancers)
   cancA=A[rep(seq_len(nrow(A)), times=A$cancers),]%>%select(-cancers)
   head(cancA,10)
   # the approach in this chunk is way too slow
@@ -23,40 +24,74 @@ simSeerSet<-function(N=2e9,yearEnd=2012,ka=1e-5,kb=0.04,Ab=1e-5,Rba=1e-3,delay=1
   #     if (d$cancers>0) for (i in 1:d$cancers) D=rbind(D,d)
   #     D   }
   #   cancA=A%>%do(myGrow(.))%>%select(-cancers)
-  cancA$surv=rexp(dim(cancA)[1],rate=0.2)
+  #   cancA$surv=rexp(dim(cancA)[1],rate=0.1)
+  cancA$surv=runif(dim(cancA)[1],0,20)
   #   mean(canc$surv)
   #   median(canc$surv)
   cancA$cancer="A"
   cancA$yrdx=cancA$year+runif(dim(cancA)[1],max=0.9999)
   cancA$casenum=1:dim(cancA)[1]
   cancA$seqnum=0
+  
+  
+  ######first treatment induced B cases
   py=cancA$surv
   head(py,40)
-  py=ifelse(py<delay,0,py-delay)
-  py=ifelse(py>period,period,py)
+  py=ifelse(py<=delay,0,py-delay)
+  py=ifelse(py>=period,period,py)
+  sum(py==0)
+  #   subCancA=cancA[py>0,]
+  #   py=py[py>0,]
+  #   head(py,40)
+  pyi=floor(py)
+  #   sum(pyi==1)
+  pyr=py-pyi
+  #   head(pyi,40)
+  #   head(pyr,40)
   #    cancA$seconds=rpois(dim(cancA)[1],Rba*py)
-  cancA$seconds=rpois(dim(cancA)[1],5*Ab*exp(kb*(cancA$age+(py+delay)/2))*py) #induced is 500x > background risk
+  #   cancA$seconds=rpois(dim(cancA)[1],5*Ab*exp(kb*(cancA$age+(py+delay)/2))*py) #induced is 5x > background risk
+  myexpi=function(age1,pyi1) sum(exp(kb*(age1+0.5+delay + 0:(pyi1-1))))
+  expis=mapply(myexpi,cancA$age,pyi)
+  myexpr=function(age1,pyi1,pyr1) pyr1*exp(kb*(age1+delay+pyi1+pyr1/2))
+  exprs=mapply(myexpr,cancA$age,pyi,pyr)
+  cancA$seconds=rpois(dim(cancA)[1],5*Ab*(expis+exprs) ) 
   sum(cancA$seconds)
+  table(cancA$seconds,cut(py,breaks=seq(0,4,0.1)))
   cancA$seqnum[cancA$seconds>0]=1
   cancBA=cancA[cancA$seconds>0,]
   cancBA$seqnum=2
   cancBA$cancer="B"
-  #   cancBA$surv=delay+period+1 # big enough to die after the cancer
   #   cancBA$yrdx=cancBA$yrdx+delay+period*rbeta(dim(cancBA)[1],shape,shape)
-  #   cancBA$yrdx=cancBA$yrdx+runif(dim(cancBA)[1],delay,delay+period)
   #   cancBA$yrdx=cancBA$yrdx+pmin(runif(dim(cancBA)[1],delay,delay+period),cancBA$surv)
-  cancBA$yrdx=cancBA$yrdx+pmin(delay+period*rbeta(dim(cancBA)[1],shape,shape),cancBA$surv)
+  #   cancBA$yrdx=cancBA$yrdx+runif(dim(cancBA)[1],delay+1e-4,delay+period-1e-4)
+  cancBA$yrdx=cancBA$yrdx+runif(dim(cancBA)[1],delay,     delay+period)
   cancBA$seconds=NULL
   cancA$seconds=NULL
   #   hist(rbeta(1e4,1,1))
-  #   hist(rbeta(1e4,2,2))
   #    hist(rbeta(1e4,4,4))
-  #    hist(rbeta(1e4,40,40))
-  #     hist(rbeta(1e4,0,0))
-  #   head(cancBA)
-  #   head(cancA[60:65,])
   cancA=rbind(cancA,cancBA)
   
+  ######now background B after A cases
+  py=cancA$surv
+  pyi=floor(py)
+  pyr=py-pyi
+  expis=mapply(myexpi,cancA$age,pyi)
+  exprs=mapply(myexpr,cancA$age,pyi,pyr)
+  cancA$seconds=rpois(dim(cancA)[1],Ab*(expis+exprs) ) 
+#   sum(cancA$seconds)
+#   table(cancA$seconds,cut(py,breaks=seq(0,4,0.1)))
+  cancA$seqnum[cancA$seconds>0]=1
+  cancBA=cancA[cancA$seconds>0,]
+  cancBA$seqnum=2
+  cancBA$cancer="B"
+  cancBA$yrdx=cancBA$yrdx+runif(dim(cancBA)[1],0,py)
+  cancBA$seconds=NULL
+  cancA$seconds=NULL
+# head(cancBA)
+# head(cancA)
+  cancA=rbind(cancA,cancBA)
+  
+  ######now background B
   B=cbind(popsa[,1:2],cancers=rpois(dim(popsa)[1],Ab*exp(kb*popsa$age)*popsa$py))
   cancB=B[rep(seq_len(nrow(B)), times=B$cancers),]%>%select(-cancers)
   cancB$surv=rexp(dim(cancB)[1],rate=0.5)
@@ -65,6 +100,7 @@ simSeerSet<-function(N=2e9,yearEnd=2012,ka=1e-5,kb=0.04,Ab=1e-5,Rba=1e-3,delay=1
   cancB$casenum=1:dim(cancB)[1]
   cancB$seqnum=0
   
+  ######  merge A and B
   canc=rbind(cancA,cancB)
   canc$trt="noRad"
   canc$trt=factor(canc$trt)
@@ -78,19 +114,5 @@ simSeerSet<-function(N=2e9,yearEnd=2012,ka=1e-5,kb=0.04,Ab=1e-5,Rba=1e-3,delay=1
   seerSet=list(canc=canc,popsa=popsa,ageStart=min(popsa$age),ageEnd=max(popsa$age),sex="neut",race="neut",
                cancerS=cancerS,yearEnd=max(popsa$year))
   class(seerSet)="seerSet"
-  seerSet=mk2D(seerSet,secondS="B") 
-  
-  
-  cancA$seconds=rpois(dim(cancA)[1],Ab*exp(kb*(cancA$age+(cancA$surv)/2))*cancA$surv) 
-  sum(cancA$seconds)
-  cancBA=cancA[cancA$seconds>0,]
-  cancBA$seqnum=2
-  cancBA$cancer="B"
-  cancBA$yrdx=cancBA$yrdx+cancBA$surv*rbeta(dim(cancBA)[1],shape,shape)
-  cancBA$seconds=NULL
-  cancBA$trt="noRad"
-  head(cancBA)
-  head(seerSet$canc)
-  seerSet$canc=rbind(seerSet$canc,cancBA)
   seerSet
 } # return a list that can be attached or with-ed in other functions
