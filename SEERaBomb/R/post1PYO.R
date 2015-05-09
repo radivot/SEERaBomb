3de6686e9067a28d364ec28af90e9194737f7c94
radivot <radivot@lri-106018.attlocal.net>
2015-05-08 20:17:24 -0400

    split D12 for cancers versus PY contributions

--- SEERaBomb/R/post1PYO.R
+++ SEERaBomb/R/post1PYO.R
@@ -24,6 +24,7 @@ post1PYO=function(canc,brks=c(0,2,5),binIndx=1,Trt="rad",yearEnd ) {
   names(D1)[2:5]=c("cancer1","yrdx1","age1","trt1") #rename D1 cols so as not to join by them.
   # D2 colnames  db  casenum	reg	race	sex	agedx	yrbrth	seqnum	modx	yrdx	radiatn	surv	cancer	trt
   #   D2=D2%>%select(casenum,cancer,yrdx,agedx,sex,race,yrbrth) # reduce D2 to cols we want to slap on 
+#   D2=D2%>%filter(casenum%in%D1$casenum) # no diff in simulation .. filt aft leftjoin took care of it 
   D2=D2%>%select(casenum,cancer,yrdx,age) # reduce D2 to cols we want to slap on 
   names(D2)[2:4]=c("cancer2","yrdx2","age2") # and rename cols not to join by them
 #   print("inPost1")
@@ -34,28 +35,21 @@ post1PYO=function(canc,brks=c(0,2,5),binIndx=1,Trt="rad",yearEnd ) {
   D12=D12%>%filter(!is.na(yrdx1)) # removes firsts before 1973
   D12=D12%>%mutate(yrdiffn=yrdx2-yrdx1)
   D12$yrdiffn[D12$yrdiffn==0]=0.33/12 # if first and second are in the same month, assume 1/3 of a month apart
-  D12=D12%>%mutate(yrdiff=cut(yrdiffn,breaks=c(-1,brks,100),include.lowest = TRUE))
-  #   D12=D12%>%mutate(yrdiff=cut(yrdx2-yrdx1,breaks=c(-1,brks,100),include.lowest = TRUE))
-  D12=D12%>%filter(yrdiff==bin)
-#   print("here2")
-#   PY0=D0%>%mutate(py=getPY(surv,bin,binS,brks),ageL=agedx+brks[binIndx],year=floor(yrdx+brks[binIndx])) 
+  D12py=D12 #next 2-lines get cancers observed right, but PY of earlier intervals need to be delivered too
+  D12=D12%>%mutate(yrdiff=cut(yrdiffn,breaks=c(-1,brks,100),include.lowest = TRUE)) 
+  D12=D12%>%filter(yrdiff==bin) 
   PY0=D0%>%mutate(py=getPY(surv,bin,binS,brks),ageL=age+brks[binIndx],year=floor(yrdx+brks[binIndx])) 
-  PY1=D12%>%mutate(py=getPY(yrdiffn,bin,binS,brks),ageL=age1+brks[binIndx],year=floor(yrdx1+brks[binIndx])) 
-  #   PY1=D12%>%mutate(py=getPY(yrdx2-yrdx1,bin,binS,brks),ageL=agedx1+brks[binIndx],year=floor(yrdx1+brks[binIndx])) 
+  PY1=D12py%>%mutate(py=getPY(yrdiffn,bin,binS,brks),ageL=age1+brks[binIndx],year=floor(yrdx1+brks[binIndx])) 
   PY=rbind(PY0%>%mutate(cancer1=cancer,cancer2="none")%>%select(cancer1,cancer2,py,ageL,year),
            PY1%>%select(cancer1,cancer2,py,ageL,year))
-  #       head(PY)
   n=dim(PY)[1]
   binMidPnt=LL+sum(PY$py)/n/2
   PY=PY%>%mutate(ageR=ageL+py)
   PYin=PY%>%select(-cancer1,-cancer2,-ageR)
-#   print("herep3")
   LPYin=split(PYin,PY$cancer1) #splitting done on first cancers, so resulting list names are of first cancers
-#   print("here3")
   LPYinM=lapply(LPYin,as.matrix)
   LPYM=NULL
   # creat a matrix of zeros that is repeatedly the starting point of age-year PY accrual
-  #    yrs=1973:2012
   yrs=1973:yearEnd
   nyears=length(yrs)
     ages=0.5:125.5
