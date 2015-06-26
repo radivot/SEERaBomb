c37a892bbbfb4aeefb75e657c98bef49ae3b6404
radivot <radivot@lri-106018.ccf.org>
2015-06-25 21:25:48 -0400

    post1PYO now gets quantiles of py and average ages and their CI. The goal is to get to a point where PYM is never true in calls of tsd(), since this makes the seerSet objects too big to store on the HD.

--- SEERaBomb/R/post1PYO.R
+++ SEERaBomb/R/post1PYO.R
@@ -1,35 +1,35 @@
-post1PYO=function(canc,brks=c(0,2,5),binIndx=1,Trt="rad",yearEnd ,firstS,secondS) { 
+post1PYO=function(canc,brks=c(0,2,5),binIndx=1,Trt="rad",PYM=FALSE,yearEnd ,firstS,secondS) { 
   # to get rid of check notes. Using list inputs and with will shield these
   #   surv=yrdx=modx=db=casenum=radiatn=cancer=trt=yrbrth=agedx=seqnum=sex=race=reg=yrdx1=yrdiff=NULL 
-#   surv=yrdx=db=casenum=cancer=trt=agedx=yrdx1=seqnum=yrdiff=NULL 
-#   yrdx2=yrdiffn=cancer1=cancer2=py=year=ageL=ageR=agedx1=NULL 
-   surv=yrdx=age=casenum=cancer=trt=yrdx1=seqnum=yrdiff=NULL 
+  #   surv=yrdx=db=casenum=cancer=trt=agedx=yrdx1=seqnum=yrdiff=NULL 
+  #   yrdx2=yrdiffn=cancer1=cancer2=py=year=ageL=ageR=agedx1=NULL 
+  surv=yrdx=age=casenum=cancer=trt=yrdx1=seqnum=yrdiff=NULL 
   age=NULL 
-  yrdx2=yrdiffn=cancer1=cancer2=py=year=ageL=ageR=age1=NULL 
+  yrdx2=yrdiffn=cancer1=cancer2=py=year=ageL=ageR=age1=ageM=sem=NULL 
   if(sum(canc$trt==Trt,na.rm=T)==0) stop(paste0("canc must have a trt column containing",Trt))
   
   binS=levels(cut(brks+0.1,breaks=c(brks,100)))
   bin=binS[binIndx]
   #   print(bin)
-#   print("inPost")
+  #   print("inPost")
   (LL=getBinInfo(bin,binS)["LL"])
   canc=canc%>%filter()
   D0=canc%>%filter(seqnum==0,surv<200,surv>LL,trt==Trt,cancer%in%firstS)
   D0$cancer=factor(D0$cancer,levels=firstS) # get rid of levels not in firstS. 
-# need levels above since apparently pituitary is never irradiated
+  # need levels above since apparently pituitary is never irradiated
   D1=canc%>%filter(seqnum==1,trt==Trt,cancer%in%firstS)%>%select(casenum,cancer,yrdx,age,trt)  
   D1$cancer=factor(D1$cancer,levels=firstS) # get rid of levels not in firstS
   D2=canc%>%filter(seqnum==2) # D2 holds second primaries
   D1=D1%>%filter(casenum%in%D2$casenum) # reduce firsts to just those with a second in D2 
   names(D1)[2:5]=c("cancer1","yrdx1","age1","trt1") #rename D1 cols so as not to join by them.
-#   D2=D2%>%filter(casenum%in%D1$casenum) # no diff in simulation .. filt aft leftjoin took care of it 
+  #   D2=D2%>%filter(casenum%in%D1$casenum) # no diff in simulation .. filt aft leftjoin took care of it 
   D2=D2%>%select(casenum,cancer,yrdx,age) # reduce D2 to cols we want to slap on 
   names(D2)[2:4]=c("cancer2","yrdx2","age2") # and rename cols not to join by them
-#   print("inPost1")
+  #   print("inPost1")
   head(D1)
   head(D2)
   D12=left_join(D2,D1,by="casenum") #Keeps all D2 rows, inserts missing where D1 doesn't match. 
-#   print("inPost2")
+  #   print("inPost2")
   D12=D12%>%filter(!is.na(yrdx1)) # removes firsts before 1973
   D12=D12%>%mutate(yrdiffn=yrdx2-yrdx1)
   D12$yrdiffn[D12$yrdiffn==0]=0.33/12 # if first and second are in the same month, assume 1/3 of a month apart
@@ -42,7 +42,16 @@ post1PYO=function(canc,brks=c(0,2,5),binIndx=1,Trt="rad",yearEnd ,firstS,secondS
            PY1%>%select(cancer1,cancer2,py,ageL,year))
   n=dim(PY)[1]
   binMidPnt=LL+sum(PY$py)/n/2
-  PY=PY%>%mutate(ageR=ageL+py)
+  PY=PY%>%mutate(ageR=ageL+py,ageM=ageL+py/2)
+  PYQ=PY%>%group_by(cancer1)%>%summarize(Q1=quantile(py,0.25),Q2=quantile(py,0.5),Q3=quantile(py,0.75))
+  AgeE=PY%>%group_by(cancer1)%>%summarize(age=mean(ageM),sem=sd(ageM)/sqrt(n()),ci95=qt(0.975,n()-1)*sem)
+  AgeO=PY%>%filter(cancer2%in%secondS)%>%group_by(cancer1,cancer2)%>%
+    summarize(age=mean(ageR),sem=sd(ageR)/sqrt(n()),ci95=qt(0.975,n()-1)*sem)
+#   AgeE=PY%>%group_by(cancer1,cancer2)%>%
+#     summarize(age=mean(ageL)+binMidPnt,L=t.test$conf.int[1],L=t.test(ageL)$conf.int[2])%>%filter(cancer2%in%secondS)
+#   AgeO=PY%>%filter(cancer2%in%secondS)%>%group_by(cancer1,cancer2)%>%
+#     summarize(age=mean(ageR),L=t.test(ageR)$conf.int[1],L=t.test$conf.int[2])%>%filter(cancer2%in%secondS)
+  
   PYin=PY%>%select(-cancer1,-cancer2,-ageR)
   LPYin=split(PYin,PY$cancer1) #splitting done on first cancers, so resulting list names are of first cancers
   LPYinM=lapply(LPYin,as.matrix)
@@ -50,45 +59,47 @@ post1PYO=function(canc,brks=c(0,2,5),binIndx=1,Trt="rad",yearEnd ,firstS,secondS
   # creat a matrix of zeros that is repeatedly the starting point of age-year PY accrual
   yrs=1973:yearEnd
   nyears=length(yrs)
-    ages=0.5:125.5
-    Zs=matrix(0,ncol=nyears,nrow=length(ages))
-    colnames(Zs)=yrs
-    rownames(Zs)=ages
-#     head(Zs)
+  ages=0.5:125.5
+  Zs=matrix(0,ncol=nyears,nrow=length(ages))
+  colnames(Zs)=yrs
+  rownames(Zs)=ages
+  #     head(Zs)
   #   print(tail(D12,2))
-#   print(Trt)
+  #   print(Trt)
   print(bin)
-#   print(sapply(LPYinM,dim))
-#   print(dim(sapply(LPYinM,dim)))
+  #   print(sapply(LPYinM,dim))
+  #   print(dim(sapply(LPYinM,dim)))
   
-#   #  print(gc())
-  for (i in names(LPYinM)) {
-      PYMat=Zs+0   # fake out system to allocate fresh memory for each instance of the matrix, i.e. each first cancer
-#       print(i)
-#       print(tail(LPYinM[[i]]))
-  #     print(dim(LPYinM[[i]]))
-  #       print(head(LPYinM[[i]],2))
-  #      print(tail(LPYinM[[i]]))
-  #     print(head(PYM))
-#         if(dim(LPYinM[[i]])[1]==0) LPYM[[i]]=as.matrix(data.frame(py=c(.1,.1),ageL=50,year=2000)) # set up dummy matrix
-         LPYM[[i]]=fillPYM(LPYinM[[i]],PYMat)
-#          LPYM[[i]]=mkPYM(LPYinM[[i]],nyears)
-#         LPYM[[i]]=mkPYMR(LPYinM[[i]],yearEnd)
-  #     print(head(LPYM[[i]]))
-  #       rm(PYM) 
-  #       gc()
-    } 
-#    LPYM=lapply(LPYinM,mkPYM,nyears)
-#   LPYM=lapply(LPYinM,mkPYMR,yearEnd)
-#   print("doneLapply")
+  #   #  print(gc())
+  #   for (i in names(LPYinM)) {
+  for (i in firstS) {
+    PYMat=Zs+0   # fake out system to allocate fresh memory for each instance of the matrix, i.e. each first cancer
+    #       print(i)
+    #       print(tail(LPYinM[[i]]))
+    #     print(dim(LPYinM[[i]]))
+    #       print(head(LPYinM[[i]],2))
+    #      print(tail(LPYinM[[i]]))
+    #     print(head(PYM))
+    #         if(dim(LPYinM[[i]])[1]==0) LPYM[[i]]=as.matrix(data.frame(py=c(.1,.1),ageL=50,year=2000)) # set up dummy matrix
+    LPYM[[i]]=fillPYM(LPYinM[[i]],PYMat)
+    #          LPYM[[i]]=mkPYM(LPYinM[[i]],nyears)
+    #         LPYM[[i]]=mkPYMR(LPYinM[[i]],yearEnd)
+    #     print(head(LPYM[[i]]))
+    #       rm(PYM) 
+    #       gc()
+  } 
+  #    LPYM=lapply(LPYinM,mkPYM,nyears)
+  #   LPYM=lapply(LPYinM,mkPYMR,yearEnd)
+  #   print("doneLapply")
   
   LD12=split(D12,D12$cancer1) # for getting observed cases in this interval later. Split on first => list names of firsts
   #   lapply(LD12,function(x) table(x$cancer2)) #thyroid first yields 2 AML seconds and 25 thyroid
   O=t(sapply(LD12,function(x) table(x$cancer2)))
   rownames(O)=names(LD12)
   colnames(O)=levels(D12$cancer2)
-#   print("here")
-  list(LPYM=LPYM,O=O,binMidPnt=binMidPnt,PY=PY)
+  #   print("here")
+  if (PYM) return(list(LPYM=LPYM,O=O,binMidPnt=binMidPnt,AgeE=AgeE,AgeO=AgeO,PYQ=PYQ,PY=PY))  # no speed gain with this
+  else return(list(LPYM=LPYM,O=O,binMidPnt=binMidPnt,AgeE=AgeE,AgeO=AgeO,PYQ=PYQ))
 }
 
 
--- SEERaBomb/R/tsd.R
+++ SEERaBomb/R/tsd.R
@@ -1,10 +1,10 @@
 tsd=function(seerSet,brks=c(0,2,5),trts=NULL,PYM=FALSE,firstS="all"){ 
-#   surv=yrdx=modx=db=casenum=radiatn=cancer=trt=yrbrth=agedx=L2D=NULL
+  #   surv=yrdx=modx=db=casenum=radiatn=cancer=trt=yrbrth=agedx=L2D=NULL
   print(binS<-levels(cut(brks+0.1,breaks=c(brks,100)))) #this is just to make a vector of tsd interval/row names 
   ptm <- proc.time()
   if(is.null(seerSet$L)) seerSet$L=list() # set list L to a blank list if it was never filled for this seerSet
   SL=with(seerSet, {  # subL (List within L), i.e. L is a list of time series sublists (SL). 
-#     yearEnd=max(popsa$year)
+    #     yearEnd=max(popsa$year)
     SL=list() # initiate the SL that tsd() adds to L
     if (firstS[1]=="all") firstS=cancerS
     SL$firstS=firstS
@@ -14,40 +14,49 @@ tsd=function(seerSet,brks=c(0,2,5),trts=NULL,PYM=FALSE,firstS="all"){
     for (R in trts) 
     { 
       #     R="rad"
-       print(R)
+      print(R)
       mids=NULL
       Obs=vector(mode="list",length=0)
       Exp=vector(mode="list",length=0)
-      PyM=vector(mode="list",length=0)
+      AgeO=vector(mode="list",length=0)  # mean age of observed cases
+      AgeE=vector(mode="list",length=0)  # mean age of expected cases, i.e. of PY at risk
+      PYQ=vector(mode="list",length=0)
+      if (PYM) PyM=vector(mode="list",length=0)
       for (bin in binS) 
       {
         #       (bin=binS[1])
-#         print(bin)
+        #         print(bin)
         binIndx=getBinInfo(bin,binS)["index"]
-        L1=post1PYO(canc,brks,binIndx,Trt=R,yearEnd,firstS,secondS=secondS)
+        L1=post1PYO(canc,brks,binIndx,Trt=R,PYM=PYM,yearEnd,firstS,secondS=secondS)
         Exp[[bin]]=getE(L1$LPYM,D,ageStart,ageEnd,yearEnd,firstS,secondS)
-#          Obs[[bin]]=L1$O
+        #          Obs[[bin]]=L1$O
         Obs[[bin]]=L1$O[firstS,secondS,drop=FALSE]
-#         rws=rownames(L1$O)
-#         cols=colnames(L1$O)
-#         Obs[[bin]]=L1$O[rws%in%firstS,cols%in%secondS,drop=FALSE]
-        PyM[[bin]]=L1$PY
+        AgeE[[bin]]=L1$AgeE
+        AgeO[[bin]]=L1$AgeO
+        PYQ[[bin]]=L1$PYQ
+        #         rws=rownames(L1$O)
+        #         cols=colnames(L1$O)
+        #         Obs[[bin]]=L1$O[rws%in%firstS,cols%in%secondS,drop=FALSE]
+        if (PYM) PyM[[bin]]=L1$PY
         mids=c(mids,L1$binMidPnt)
       } # loop on tsx bins
       SL[[R]]$mids=mids
       SL[[R]]$Obs=Obs
       SL[[R]]$Exp=Exp
+      SL[[R]]$AgeE=AgeE
+      SL[[R]]$AgeO=AgeO
+      SL[[R]]$PYQ=PYQ
       if (PYM) SL[[R]]$PyM=PyM
     } # loop on R
-     SL
-   })
+    SL
+  })
   tsdn=paste0("b",paste(brks,collapse="_"))
-#   seerSet$bfn=paste0(seerSet$bfn,paste0("b",paste(brks,collapse="_")),txt)
+  #   seerSet$bfn=paste0(seerSet$bfn,paste0("b",paste(brks,collapse="_")),txt)
   seerSet$L[[tsdn]]=SL
-#   fn=paste0(outDir,"/",seerSet$bfn,".RData")
-#   cat("Writing L to ",fn,"\n")
-#   save(L,file=fn)
-#   seerSet$fL=fn
+  #   fn=paste0(outDir,"/",seerSet$bfn,".RData")
+  #   cat("Writing L to ",fn,"\n")
+  #   save(L,file=fn)
+  #   seerSet$fL=fn
   print(proc.time() - ptm)
   seerSet
 }
