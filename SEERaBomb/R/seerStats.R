79b72ce9a987d90f7d005d4c69ffa829984f9ffa
radivot <radivot@lri-106018.attlocal.net>
2015-06-29 07:23:15 -0400

    added cases by seqnum columns to seerStat output

--- SEERaBomb/R/seerStats.R
+++ SEERaBomb/R/seerStats.R
@@ -6,21 +6,30 @@ seerStats<-function(canc,popsa) {
   L$P=popsa%>%group_by(db,reg)%>%summarize(PY=round(sum(py)/1e6,1),age=weighted.mean(age,py))
   D=canc%>%group_by(cancer,db)%>%summarize(n=n())
   A=canc%>%filter(agedx>99.5)%>%group_by(cancer)%>%summarize(n=n())
+  canc$seqnum=ifelse(canc$seqnum>3,4,canc$seqnum)
+  canc$seqnum=ifelse(canc$seqnum==0,1,canc$seqnum)
+  Sq=canc%>%group_by(cancer,seqnum)%>%summarize(n=n())
    C=dcast(D,cancer~db,value.var="n",fun.aggregate = sum,margins=c("db"))
 #   C=dcast(D,cancer~db,value.var="n",fun.aggregate = sum,margins=TRUE)
   O=dcast(A,cancer~.,value.var="n")
+   S=dcast(Sq,cancer~seqnum,value.var="n",fun.aggregate = sum,margins=c("seqnum")) #checks fine with total
+#    S=dcast(Sq,cancer~seqnum,value.var="n")
   d=left_join(C,O,by="cancer")
-  names(d)[5:6]=c("total","over99")
-  d=d%>%mutate(under100=total-over99)
+  names(d)[5:6]=c("total","gte100")
+  d=d%>%mutate(under100=total-gte100)
+  d=left_join(d,S,by="cancer")
+  names(d)[8:12]=c("firsts","seconds","thirds","highers","all")
   d[is.na(d)]=0
   d$cancer=as.character(d$cancer)
   d[dim(d)[1]+1,]=data.frame("total",t(sapply(d[-1],sum)))
   d[dim(d)[1],1]="total"
   L$d=d
-  cat(paste("Cases per SEER database through",max(popsa$year),"(sexes and races pooled).\n"))
+  cat(paste("\nTable 1. Cases per database, by >= or < 100, and by cancer sequence, through",
+            max(popsa$year),"(sexes and races pooled).\n"))
   print(d) 
-  print(L$P) 
-  cat(paste("Penultimate column: PY in millions are through",max(popsa$year),"and sexes and races pooled.\n"))
-  cat(paste("Last column: average ages are PY-weighted.\n"))
+  cat(paste("\nTable 2. Population PY in millions and PY-weighted mean ages, through",
+             max(popsa$year),"(sexes and races pooled).\n"))
+  cat(paste(""))
+  print(as.data.frame(L$P)) 
   invisible(L)
 } 
