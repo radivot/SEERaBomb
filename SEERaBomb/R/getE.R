d7fc6df104b77c638abb938b82ed6720295f0079
Tom Radivoyevitch <radivot@gmail.com>
2016-06-06 00:02:18 -0400

    updated getE and mk2D to handle cancers that entered SEER in 2010 (motivated by LGL)

--- SEERaBomb/R/getE.R
+++ SEERaBomb/R/getE.R
@@ -6,27 +6,41 @@ getE=function(LPYM,D,ageStart,ageEnd,yearEnd,firstS,secondS) {
   LPYM=lapply(LPYM,function(x) x[-c((ageEnd+1):126),])
 #   LPYM=lapply(LPYM,function(x) x[-c(1:15,86:126),-dim(x)[2]])
   LPYM[["CMML"]]=LPYM[["CMML"]][,-c(1:13)] # shave off early years when wasn't included in SEER
-  LPYM[["MDS"]]=LPYM[["MDS"]][,-c(1:28)]
+  LPYM[["MDS"]]=LPYM[["MDS"]][,-c(1:28)]  # starts in 2001
   LPYM[["MPN"]]=LPYM[["MPN"]][,-c(1:28)]
   LPYM[["RARS"]]=LPYM[["RARS"]][,-c(1:28)]
   LPYM[["AMLti"]]=LPYM[["AMLti"]][,-c(1:28)]
   LPYM[["unknown"]]=LPYM[["unknown"]][,-c(1:28)]
+  LPYM[["LGL"]]=LPYM[["LGL"]][,-c(1:37)] #LGL starts in 2010
 #    secondS=names(EI)
   strt01=c("MDS","MPN","RARS","AMLti","unknown")
+  strt10=c("LGL")
   (E=matrix(0,nrow=length(firstS),ncol=length(secondS),dimnames=list(firstCanc=firstS,secondCanc=secondS)))
   for (i in firstS) {   #i loop on first cancers
     PY=LPYM[[i]]
     for (j in secondS) # j loop on second cancers
     { 
-      if (i=="CMML" & !j%in%c("CMML",strt01))  E[i,j]=sum(PY*EI[[j]][,as.character(1986:yearEnd)])
+      # if (i=="CMML" & !j%in%c("CMML",strt01))  E[i,j]=sum(PY*EI[[j]][,as.character(1986:yearEnd)])
+      if (i=="CMML" & !j%in%c("CMML",strt01,strt10))  E[i,j]=sum(PY*EI[[j]][,as.character(1986:yearEnd)])
       if (i=="CMML" & j=="CMML")  E[i,j]=sum(PY*EI[[j]])
       if (i=="CMML" & j%in%strt01)  E[i,j]=sum(PY[,as.character(2001:yearEnd)]*EI[[j]])
-      if (i%in%strt01 & !j%in%c("CMML",strt01))  E[i,j]=sum(PY*EI[[j]][,as.character(2001:yearEnd)])
+      if (i=="CMML" & j%in%strt10)  E[i,j]=sum(PY[,as.character(2010:yearEnd)]*EI[[j]])
+      
+      # if (i%in%strt01 & !j%in%c("CMML",strt01))  E[i,j]=sum(PY*EI[[j]][,as.character(2001:yearEnd)])
+      if (i%in%strt01 & !j%in%c("CMML",strt01,strt10))  E[i,j]=sum(PY*EI[[j]][,as.character(2001:yearEnd)])
       if (i%in%strt01 & j=="CMML")  E[i,j]=sum(PY*EI[[j]][,as.character(2001:yearEnd)])
       if (i%in%strt01 & j%in%strt01 )  E[i,j]=sum(PY*EI[[j]])
-      if (!i%in%c("CMML",strt01) & j%in%strt01 )  E[i,j]=sum(PY[,as.character(2001:yearEnd)]*EI[[j]])
-      if (!i%in%c("CMML",strt01) & j=="CMML" )  E[i,j]=sum(PY[,as.character(1986:yearEnd)]*EI[[j]])
-      if (!i%in%c("CMML",strt01) & !j%in%c("CMML",strt01) )  E[i,j]=sum(PY*EI[[j]])
+      if (i%in%strt01 & j%in%strt10)  E[i,j]=sum(PY[,as.character(2010:yearEnd)]*EI[[j]])
+      
+      if (i%in%strt10 & !j%in%c("CMML",strt01,strt10))  E[i,j]=sum(PY*EI[[j]][,as.character(2010:yearEnd)])
+      if (i%in%strt10 & j=="CMML")  E[i,j]=sum(PY*EI[[j]][,as.character(2010:yearEnd)])
+      if (i%in%strt10 & j%in%strt01 )  E[i,j]=sum(PY*EI[[j]][,as.character(2010:yearEnd)])
+      if (i%in%strt10 & j%in%strt10)  E[i,j]=sum(PY*EI[[j]])
+      
+      if (!i%in%c("CMML",strt01,strt10) & j%in%strt10 )  E[i,j]=sum(PY[,as.character(2010:yearEnd)]*EI[[j]])
+      if (!i%in%c("CMML",strt01,strt10) & j%in%strt01 )  E[i,j]=sum(PY[,as.character(2001:yearEnd)]*EI[[j]])
+      if (!i%in%c("CMML",strt01,strt10) & j=="CMML" )  E[i,j]=sum(PY[,as.character(1986:yearEnd)]*EI[[j]])
+      if (!i%in%c("CMML",strt01,strt10) & !j%in%c("CMML",strt01,strt10) )  E[i,j]=sum(PY*EI[[j]])
     }
   }   
   E/1e5
--- SEERaBomb/R/mk2D.R
+++ SEERaBomb/R/mk2D.R
@@ -1,6 +1,6 @@
 mk2D<-function(seerSet, knots=5, write=FALSE, outDir="~/Results", txt=NULL,secondS=NULL) {
   if(!file.exists(outDir))  {   print(paste("Creating directory",outDir))
-                                dir.create(outDir,recursive=TRUE)    }
+    dir.create(outDir,recursive=TRUE)    }
   #   require(dplyr)
   #   require(mgcv) # Mixed GAM Computation Vehicle with GCV/AIC/REML smoothness estimation
   ptm <- proc.time()
@@ -17,13 +17,19 @@ mk2D<-function(seerSet, knots=5, write=FALSE, outDir="~/Results", txt=NULL,secon
       if (i=="otherCIS" & knots<10) knots=10   # 5 doesn't cut it for this, 10 does
       if (i=="liver" & knots<10) knots=10   # same here, to capture the cohort effect ripple
       if (i=="APL" & knots>5) knots=5   # here too many knots dive down too much into early calendary years of too few cases
+      if (i=="LGL" & knots>5) knots=3   # here too many knots dive down too much into early calendary years of too few cases
       #the next if else deals with MDS and CMML starting only later. Not sure what other cancers are like this.
-# if (i=="MDS")  {d=canc%>%filter(cancer%in%i,year>2000)
-      if (i%in%c("MDS","MPN","RARS","AMLti","unknown"))  {d=canc%>%filter(cancer%in%i,year>2000)
-                                 ps=popsa%>%filter(year>2000)
+      # if (i=="MDS")  {d=canc%>%filter(cancer%in%i,year>2000)
+      if (i%in%c("LGL"))  {
+        d=canc%>%filter(cancer%in%i,year>2009)
+        ps=popsa%>%filter(year>2009)
+      } else 
+        if (i%in%c("MDS","MPN","RARS","AMLti","unknown"))  {
+        d=canc%>%filter(cancer%in%i,year>2000)
+        ps=popsa%>%filter(year>2000)
       } else 
         if (i=="CMML")  {d=canc%>%filter(cancer%in%i,year>1985) 
-                         ps=popsa%>%filter(year>1985)
+        ps=popsa%>%filter(year>1985)
         } else  {
           d=canc%>%filter(cancer%in%i)
           ps=popsa
@@ -34,6 +40,8 @@ mk2D<-function(seerSet, knots=5, write=FALSE, outDir="~/Results", txt=NULL,secon
       X=left_join(ps,d)%>%mutate(incid=1e5*cases/py)
       X[is.na(X)]=0
       cat(knots," knots, working on cancer ",i,":\n")
+      if (i=="LGL") L2D[[i]]=gam(cases ~ s(age)+offset(log(py)),
+                   family=poisson(),data=X,method="REML") else
       L2D[[i]]=gam(cases ~ s(age)+s(year)+ti(age,year,k=knots)+offset(log(py)),
                    family=poisson(),data=X,method="REML") 
       prd=as.numeric(predict(L2D[[i]]))
