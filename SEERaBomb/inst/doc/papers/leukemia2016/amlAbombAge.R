59f9d2ccf31eca0cb096d32a76f3a2fa45499c51
Tom Radivoyevitch <radivot@gmail.com>
2016-11-28 08:17:03 -0500

    added color back to A-Bomb AML vs age vs Dose plot

--- SEERaBomb/inst/doc/papers/leukemia2016/amlAbombAge.R
+++ SEERaBomb/inst/doc/papers/leukemia2016/amlAbombAge.R
@@ -10,20 +10,22 @@ d=collect(tbl(db, sql("SELECT * from heme")))%>%
   summarise(age=mean(age),py=sum(py)/1e5,AML=sum(AMLtot),
             L=qpois(.025,AML),U=qpois(.975,AML),Incid=AML/py,IL=L/py,IU=U/py)
 d
+d$Dose=factor(d$Dose,levels=c("High","Medium","Low"))
 library(ggplot2)
 graphics.off()
 quartz(width=7,height=5)
-theme_set(theme_bw(base_size = 18))
-# theme_set(theme_gray(base_size = 18)) 
+# theme_set(theme_bw(base_size = 18))
+theme_set(theme_gray(base_size = 16)) 
 theme_update(legend.position = c(.7, .2),
              axis.text=element_text(size=rel(1.3)),
              axis.title=element_text(size=rel(1.3)),
              legend.title=element_text(size=rel(1.1)),
              legend.text=element_text(size=rel(1.1)))
 g=qplot(x=age+0.5*(as.numeric(Dose)-1),y=Incid,data=d,col=Dose,log="y",ylab="Incidence (Cases/100,000 PY)",
-        geom=c("line","point"),xlab="Age")
-g = g + scale_color_grey(start = 0.8, end = 0)
+        geom=c("line","point"),xlab="Age")+geom_line(lwd=1.3)
+# g = g + scale_color_grey(start = 0.8, end = 0)
 g+  geom_errorbar(aes(ymin=IL,ymax=IU,width=.15))
+
 ggsave("~/Results/amlMDS/abombAMLage.eps")  
 ggsave("~/Results/amlMDS/abombAMLage.png")  
 
