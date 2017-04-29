6b871c37b74534b83ffccc82e9cf95aae4b9c1bd
Tom Radivoyevitch <radivot@gmail.com>
2017-04-29 05:01:56 -0400

    added mkSEERcrt.R to examples folder

---
+++ SEERaBomb/inst/doc/examples/mkSEERcrt.R
@@ -0,0 +1,38 @@
+d=read.table("~/data/SEER/seerstatexport.txt",sep="\t")
+names(d)=c("casenum","reg","race","sex","agedx","yrbrth",
+           "seqnum","modx","yrdx","histo3","ICD9",
+           "COD","surv","RT","CT")
+crt=d%>%select(casenum,seqnum,CT,RT)
+save(crt,file="~/data/SEER/crt.RData")
+head(crt)
+load("~/data/SEER/crt.RData")
+load("~/data/SEER/mrgd/cancDef.RData")
+levels(crt$seqnum)
+head(crt$seqnum)
+x=strsplit(as.character(crt$seqnum), "[^0-9]+")
+crt$seq=sapply(x,function(y) y[[1]])
+crt$seq[crt$seqnum=="One primary only"]=0
+crt$seq[crt$seqnum=="Only one state registry-defined neoplasm"]=60
+crt$seq[crt$seqnum=="Unknown seq num - federally required in situ or malig tumors"]=99
+crt$seq[crt$seqnum=="Unknown seq num - state registry-defined neoplasms"]=88
+(S=paste0(1:27,"th of ",1:27," or more state registry-defined neoplasms"))
+for (i in 1:27)  crt$seq[crt$seqnum==S[i]] = 60+i
+crt$seq=as.integer(crt$seq)
+crt$seq[1:800]
+crt$ct=as.integer(crt$CT)-1
+levels(crt$RT)
+crt$radiatn=9  # 9 is gone, so shouldn't be any left after below
+crt$radiatn[crt$RT=="None/Unknown"]=0
+crt$radiatn[crt$RT=="Beam radiation"]=1
+crt$radiatn[crt$RT=="Radioactive implants"]=2
+crt$radiatn[crt$RT=="Radioisotopes"]=3
+crt$radiatn[crt$RT=="Combination of beam with implants or isotopes"]=4
+crt$radiatn[crt$RT=="Radiation, NOS  method or source not specified"]=5
+crt$radiatn[crt$RT=="Other radiation (1973-1987 cases only)"]=6
+crt$radiatn[crt$RT=="Refused"]=7
+crt$radiatn[crt$RT=="Recommended, unknown if administered"]=8
+head(crt,20)
+cr=crt%>%select(casenum,seqnum=seq,ct,radiatn)
+d=left_join(canc,cr)
+save(d,file="~/data/SEER/cr.RData")
+
