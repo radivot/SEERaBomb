637dba733e9218c3fb16a71bfbc28f550b9677e3
Tom Radivoyevitch <radivot@gmail.com>
2018-04-26 17:35:03 -0400

    minor tut touches

--- SEERaBomb/inst/doc/papers/tutorial/install.R
+++ SEERaBomb/inst/doc/papers/tutorial/install.R
@@ -7,15 +7,13 @@ library(SEERaBomb)   # loads the installed package SEERaBomb into memory
 (rdf=pickFields(df)) # picks a subset of SEER fields and defines their types
 mkSEER(rdf)          # makes merged data file ~/data/SEER/mrgd/cancDef.Rdata
 
-load("~/data/SEER/mrgd/cancDef.RData") #loads the object canc into memory
+load("~/data/SEER/mrgd/cancDef.RData") # loads the object canc into memory
 head(canc,3)         # returns top 3 rows of canc
-
 
 library(SEERaBomb)
 mkAbomb() # converts files in ~\data\abomb into ~\data\abomb\abomb.RData 
-To check it
-load("~/data/abomb/abomb.RData") #loads A-bomb data frames
+load("~/data/abomb/abomb.RData") # loads A-bomb data frames
 hemeDesc
 
-mkMrt("username", "password") # sub in your account info
+mkMrt("username","password") # sub in your account info
 
--- SEERaBomb/inst/doc/papers/tutorial/tMDSrepair.R
+++ SEERaBomb/inst/doc/papers/tutorial/tMDSrepair.R
@@ -1,11 +1,11 @@
-graphics.off(); rm(list=ls())  #clear plots and environment
+graphics.off(); rm(list=ls())  # clear plots and environment 
 library(tidyverse) # load R packages dplyr and ggplot2 
-load("~/data/SEER/mrgd/cancDef.RData") #load canc 
-(sc=canc%>%filter(histo3==9920|histo3==9987,yrdx>2000)) #sc = subset of canc
-(d=sc%>%count(sex,yrdx,histo3)) #output col n = number in sex-year-ICDO3 group 
-if(.Platform$OS.type=="windows") quartz<-function() windows() 
-quartz(width=3,height=2.5) # line above => this works on Windows
+load("~/data/SEER/mrgd/cancDef.RData") # load canc 
+(sc=canc%>%filter(histo3==9920|histo3==9987,yrdx>2000)) # sc = subset of canc
+(d=sc%>%count(sex,yrdx,histo3))# output col n = number in sex-year-ICDO3 group 
+quartz(width=3,height=2.5) # replace with windows() on Windows
 qplot(yrdx,n,data=d,facets=sex~histo3,ylab="Cases",xlab="Year of Diagnosis") +
-  scale_x_continuous(breaks=seq(2005,2015,5)) #+theme_bw()
-ggsave("~/Results/tutorial/tMDSrepaired.pdf")  
+  scale_x_continuous(breaks=seq(2005,2015,5))
+ggsave("~/Results/tutorial/tMDSrepaired.pdf") 
+
 
