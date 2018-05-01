116a9bd944224f01bab420197d30fddc1bd718be
Tom Radivoyevitch <radivot@gmail.com>
2018-05-01 12:00:36 -0400

    add recursive to dir.create() in mkMrt so it now makes ~\data if it isn't already there.

--- SEERaBomb/R/mkMrt.R
+++ SEERaBomb/R/mkMrt.R
@@ -1,6 +1,6 @@
 mkMrt<-function(username,passwd,country="USA",mrtHome="~/data/usMort"){
   mrtHome=path.expand(mrtHome)
-  if(!dir.exists(mrtHome)) dir.create(mrtHome)
+  if(!dir.exists(mrtHome)) dir.create(mrtHome,recursive=T)
   mrt=demography::hmd.mx(country, username, passwd)$rate 
   names(mrt)=c("Female","Male","Total")
   save(mrt,file=f<-file.path(mrtHome,"mrt.RData"))
