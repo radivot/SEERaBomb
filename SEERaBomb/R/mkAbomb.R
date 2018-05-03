44b27050f1ac8b3cdc21c907d88769a73186b89a
Tom Radivoyevitch <radivot@gmail.com>
2018-05-03 09:18:09 -0400

    rearrange abomb cols into 3 groups

--- SEERaBomb/R/mkAbomb.R
+++ SEERaBomb/R/mkAbomb.R
@@ -2,8 +2,8 @@ mkAbomb<-function(AbombHome="~/data/abomb"){
   #   library(RSQLite)
   # gimic to get rid of unwanted notes in R CMD check
   marD=py=doseg=ovaD=tesD=uteD=g=NULL #seems to be OK with D and n coming from some global source: function names?
-  
-  
+  year=city=un4gy=ageG=DG=gdist=upy=subjects=NHL=tot=marg=marn=colD=histnos=NULL
+
   AbombHome=path.expand(AbombHome)
   outDB=file.path(AbombHome,"abomb.db") 
   m <- dbDriver("SQLite")
@@ -14,6 +14,7 @@ mkAbomb<-function(AbombHome="~/data/abomb"){
     "NHL","HL","MM","ALL","OLL","ALLtot","CLL","HCL","CLLtot","ATL",
     "AML","OML","AMoL","AMLtot","CML","othleuk","nonCLL","leuktot",
     "tot","D","g","n")
+  added=c("t","ageG","agexG","tG","DG")
   desc=c(
     "Hiroshima=1, Nagasaki=2",                               "Male=1, Female=2",
     "Under 4 Gy of Shielded Kerma Total (G+N) indicator",    "Ground distance categories",
@@ -42,18 +43,32 @@ mkAbomb<-function(AbombHome="~/data/abomb"){
     "Non-CLL/non-ATL leumemia",
     "Total leukemia",
     "All events",
-    "DS02 Bone Marrow dose Sv",    "DS02 Bone Marrow Gamma",    "DS02 Bone Marrow Neutron")
-  df=data.frame(colName,desc)
+    "DS02 Bone Marrow dose Sv",    "DS02 Bone Marrow Gamma",    "DS02 Bone Marrow Neutron",
+    "time since bombing","age Group","agex Group","t Group","Dose Group")
+  nms=c(colName,added)
+  df=data.frame(nms,desc)
   d=read.csv(file.path(AbombHome,"lsshempy.csv"),col.names=colName)
-  d=d%>%
-    mutate(D=D/1000,g=g/1000,n=n/1000,t=year-1945.6)  #%.%
-#     filter(doseg>1)  #this group has negative doses, which means they are unknown, so take them out. 
-#     filter(D>=0,py>0)%.% 
-#     mutate(dose=cut(D,c(0,.02,.4,10),include.lowest=TRUE,labels=c("low","med","high"))) 
+  d=d%>%mutate(D=D/1000,g=g/1000,n=n/1000,t=year-1945.6)  #%.%
+  # head(d,2)
+  d$ageG=cut(d$age,c(0,20,40,60,70,80,110))
+  d$agexG=cut(d$agex,c(0,20,40,110))
+  d$tG=cut(d$t,c(0,10,20,40,110)) 
+  d$DG=cut(d$D,c(-1,.02,1,100),labels=c("Ctrl","<1Sv",">1Sv"))
+  d$DG=factor(d$DG,levels=c(">1Sv","<1Sv","Ctrl"))#>1Sv at top
+  # head(d,2)
+  L=as.list(desc)
+  names(L)=nms
+  labelled::var_label(d) <- L
+  # View(d)
+  d=d%>%select(city:un4gy,ageG:DG,gdist:year,D:t,upy:py,subjects,NHL:tot)
+  # head(d,2)
+  #     filter(doseg>1)  #this group has negative doses, which means they are unknown, so take them out. 
+  #     filter(D>=0,py>0)%.% 
+  #     mutate(dose=cut(D,c(0,.02,.4,10),include.lowest=TRUE,labels=c("low","med","high"))) 
   dbWriteTable(con, "heme", d,overwrite=TRUE)
   dbWriteTable(con, "hemeDesc", df,overwrite=TRUE)
   heme=as_tibble(d)
-  hemeDesc=as_tibble(df)
+  # hemeDesc=as_tibble(df)
   
   colName = c("city", "sex", "un4gy","distg","agexg","ageg","doseg",
               "calg", "subjects", "upy", "py", "gdist", "agex", "age", "year", 
@@ -90,22 +105,42 @@ mkAbomb<-function(AbombHome="~/data/abomb"){
     "renal", "Other urinary system cancer", "Cancer of Central nervous system", "thyroid", "Other solid cancer", "other sites group", 
     "adeno", "squam", "Other epithelial cancers", "sarcoma", "Other non-epithelial cancers", "Histology not otherwise specified", 
     "colD","colg","coln", "marD","marg","marn", "braD", "breD", "livD", "livg", "livn", "lunD", 
-    "ovaD", "panD", "Skeleton dose, mGy", "skiD", "stoD", "tesD", "thyD", "blaD", "uteD","truncation factor", "adjustment factor")
-  df=data.frame(colName,desc)
+    "ovaD", "panD", "Skeleton dose, mGy", "skiD", "stoD", "tesD", "thyD", "blaD", "uteD","truncation factor", "adjustment factor",
+    "time since bombing","age Group","agex Group","t Group","Dose Group")
+  
+  nms=c(colName,added)
+  df=data.frame(nms,desc)
+  # df=data.frame(colName,desc)
   #   d=read.csv(file.path(AbombHome,"lssinc07.csv"))
   #   dput(names(d))
   d=read.csv(file.path(AbombHome,"lssinc07.csv"),col.names=colName)
-  d=d%>%select(-ovaD,-tesD,-uteD)%>%mutate(t=year-1945.6)
-#   d=d%.%filter(doseg>1)%.%select(-ovaD,-tesD,-uteD)
-
-#   d=d%.%filter(marD>=0,py>0) #%.% 
+  d=d%>%select(-ovaD,-tesD,-uteD)%>%mutate(D=marD,g=marg,n=marn,t=year-1945.6)
+  # head(d,2)
+  d$ageG=cut(d$age,c(0,20,40,60,70,80,110))
+  d$agexG=cut(d$agex,c(0,20,40,110))
+  d$tG=cut(d$t,c(0,10,20,40,110)) 
+  d$DG<-cut(d$D,c(-1,.02,1,100),labels=c("Ctrl","<1Sv",">1Sv"))
+  d$DG=factor(d$DG,levels=c(">1Sv","<1Sv","Ctrl"))#>1Sv at top
+  # head(d,2)
+  L=as.list(desc)
+  names(L)=nms
+  labelled::var_label(d) <- L
+  # View(d)
+  d=d%>%select(city:un4gy,ageG:DG,gdist:year,colD:t,upy:py,subjects,solid:histnos)
+  # head(d,2)
+  
+  
+  
+  #   d=d%.%filter(doseg>1)%.%select(-ovaD,-tesD,-uteD)
+  
+  #   d=d%.%filter(marD>=0,py>0) #%.% 
   #   mutate(dose=cut(marD,c(0,.02,.4,10),include.lowest=TRUE,labels=c("low","med","high"))) 
   
   dbWriteTable(con,"solid",d,overwrite=TRUE)
   dbWriteTable(con,"solidDesc",df,overwrite=TRUE)
   solid=as_tibble(d)
-  solidDesc=as_tibble(df)
-  save(heme,hemeDesc,solid,solidDesc,file=f<-file.path(AbombHome,"abomb.RData"))
+  # solidDesc=as_tibble(df)
+  save(heme,solid,file=f<-file.path(AbombHome,"abomb.RData"))
   
   cat("The following tables:\n")
   print(dbListTables(con)); 	
--- SEERaBomb/inst/doc/papers/tutorial/abombLeu.R
+++ SEERaBomb/inst/doc/papers/tutorial/abombLeu.R
@@ -2,14 +2,17 @@ graphics.off();rm(list=ls())
 library(tidyverse)
 load("~/data/abomb/abomb.RData")
 (d=heme%>%select(age,agex,t,D,py,AML=AMLtot,ALL,CML))
-d$Dose<-cut(d$D,c(-1,.02,1,100),labels=c("Ctrl","<1Sv",">1Sv"))
-d$Dose=factor(d$Dose,levels=c(">1Sv","<1Sv","Ctrl"))#>1Sv at top
+d$DG<-cut(d$D,c(-1,.02,1,100),labels=c("Ctrl","<1Sv",">1Sv"))
+d$DG=factor(d$DG,levels=c(">1Sv","<1Sv","Ctrl"))#>1Sv at top
 d$ageG=cut(d$age,c(0,20,40,60,70,80,110))
+d$agexG=cut(d$agex,c(0,20,40,110))
+d$tG=cut(d$t,c(0,10,20,40,110)) 
+
 dA=d%>%group_by(Dose,ageG)
 (ds=dA%>%summarize_at(vars(py:CML),funs(sum)))
 (dm=dA%>%summarize_at(vars(age,D),funs(weighted.mean(.,w=py))))
 joinM=function(dm,ds) {
-  D=left_join(dm,ds)#like bind_cols() but removes exta col copies 
+  D=left_join(dm,ds)#like bind_cols() but removes extra col copies 
   D=D%>%gather(value="O",key="leuk",-(Dose:py))#O=Observed
   D$leuk=factor(D$leuk,levels=c("AML","ALL","CML"))#set order
   D%>%mutate(py=py/1e5,I=O/py,LL=qpois(0.025,O)/py,UL=qpois(0.975,O)/py)
@@ -26,7 +29,6 @@ g=ggplot(D,aes(x=age,y=I,shape=Dose,col=Dose))+geom_point()+geom_line()+
 g+gy+ge+coord_cartesian(ylim=c(.2,200))+myt
 ggsave("~/Results/tutorial/abombLeuAge.pdf",width=4,height=2.5)#Fig.5A
 
-d$agexG=cut(d$agex,c(0,20,40,110))
 dB=d%>%group_by(Dose,agexG)
 (ds=dB%>%summarize_at(vars(py:CML),funs(sum)))
 (dm=dB%>%summarize_at(vars(agex,D),funs(weighted.mean(.,w=py))))
@@ -36,7 +38,6 @@ g=ggplot(D,aes(x=agex,y=I,shape=Dose,col=Dose))+geom_point()+geom_line()+
 g+gy+ge+coord_cartesian(ylim=c(.2,200))+myt
 ggsave("~/Results/tutorial/abombLeuAgex.pdf",width=4,height=2.5)#Fig.5B
 
-d$tG=cut(d$t,c(0,10,20,40,110)) 
 dC=d%>%group_by(Dose,tG)
 (ds=dC%>%summarize_at(vars(py:CML),funs(sum)))
 (dm=dC%>%summarize_at(vars(t,D),funs(weighted.mean(.,w=py))))
