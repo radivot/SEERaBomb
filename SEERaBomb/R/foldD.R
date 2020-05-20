fcdc5da478a84f8f1ee0803e674fbe15ea023c2e
Tom Radivoyevitch <radivot@gmail.com>
2020-05-19 20:28:23 -0400

    tweak folds

--- SEERaBomb/R/foldD.R
+++ SEERaBomb/R/foldD.R
@@ -3,6 +3,6 @@ foldD=function(D,keep=c("int")) {
   O=E=EAR=PY=all_of=NULL
   D=D%>%group_by_at(vars(all_of(keep)))%>%summarize(O=sum(O),E=sum(E),PY=sum(PY),t=mean(t))
   D%>%mutate(EAR=(O-E)/PY,LL=EAR-1.96*sqrt(O)/PY,UL=EAR+1.96*sqrt(O)/PY,
-             RR=O/E,rrL=qchisq(.025,2*O)/(2*E),rrU=qchisq(.975,2*O+2)/(2*E)) 
+             RR=O/E,rrL=qchisq(.025,2*O)/(2*E),rrU=qchisq(.975,2*O+2)/(2*E))%>%filter(E>0)%>%ungroup 
 }
 
--- SEERaBomb/R/foldDF.R
+++ SEERaBomb/R/foldDF.R
@@ -1,6 +1,6 @@
 foldDF=function(DF,keep=c("cancer1","cancer2","int","trt")) { #sum over sex
   O=E=RR=all_of=NULL
   DF=DF%>%group_by_at(vars(all_of(keep)))%>%summarize(O=sum(O),E=sum(E),t=mean(t))
-  DF%>%mutate(RR=O/E,rrL=qchisq(.025,2*O)/(2*E),rrU=qchisq(.975,2*O+2)/(2*E)) 
+  DF%>%mutate(RR=O/E,rrL=qchisq(.025,2*O)/(2*E),rrU=qchisq(.975,2*O+2)/(2*E))%>%filter(E>0)%>%ungroup 
 }
 
